382858f7b40fcc4bea399bf5b9108c7e11f45a5c
Thomas Pope <tpope0928@gmail.com>
2019-09-05 02:15:38 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'secrets/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+  #Visit the root of the app and be redirected to a login page.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/welcome' => 'sessions#show'
+  post '/logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
